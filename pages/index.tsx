5f1ce8ea98a6ad54d75f8b893ca0379ae3a1bc81
Sean OReilly <soreilly424@gmail.com>
2022-05-06 12:40:07 -0500

    Adding Header to Layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,32 +4,6 @@ import Layout from '@/components/Layout'
 const Home: NextPage = () => {
   return (
     <Layout title="Crypto Clone">
-      <div className="w-full container mx-auto">
-        <nav className='flex justify-between'>
-          <ul className='flex items-center space-x-4'>
-            <li className='flex space-x-2'>
-              <span>💙</span>
-              <span>ONJUNO</span>
-            </li>
-            <li className='flex items-center space-x-1'>
-              <span>Company</span>
-              <img src="/images/icons/chevron-down.svg" alt="Chevron Down Icon" className='h-4' />
-            </li>
-            <li className='flex items-center space-x-1'>
-              <span>Learn</span>
-              <img src="/images/icons/chevron-down.svg" alt="Chevron Down Icon" className='h-4' />
-            </li>
-          </ul>
-          <ul className='flex items-center space-x-4'>
-            <li>
-              <button>Login</button>
-            </li>
-            <li>
-              <button>Create Account</button>
-            </li>
-          </ul>
-        </nav>
-      </div>
     </Layout>
   )
 }
