572e802f7c254b249c02a59284ac10f62ee31f88
Sean OReilly <soreilly424@gmail.com>
2022-06-01 13:13:57 -0500

    Added vertical gallery

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
+import VerticalGallery from '@/components/VerticalGallery'
 
 const images = [
   'https://images.unsplash.com/photo-1633109633238-e982d5b61f9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2664&q=80',
@@ -75,6 +76,7 @@ const Home: NextPage = () => {
           />
         </motion.div>
       </motion.section>
+      <VerticalGallery />
     </Layout>
   )
 }
